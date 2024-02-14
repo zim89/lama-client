feec2b966e585254b6bc4d3350fcea6a12007933
Anton <antonshestakov124@gmail.com>
2024-02-14 18:09:08 +0200

    first version

--- src/modules/HelpYouFind/HelpYouFind.tsx
+++ src/modules/HelpYouFind/HelpYouFind.tsx
@@ -2,8 +2,22 @@ import Title from './ui/title';
 
 export default function HelpYouFind() {
   return (
-    <div className='section xl:px-[7.5rem]'>
-      <Title title={'Допоможемо знайти'} />
-    </div>
+    <>
+      <div className='section xl:px-[7.5rem]'>
+        <Title title={'Допоможемо знайти'} />
+      </div>
+      <div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div></div>
+      </div>
+    </>
   );
 }
