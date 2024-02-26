df255886bfad65503fdcbf71333eff7c7fda462d
zim89 <121241841+zim89@users.noreply.github.com>
2024-02-26 22:34:41 +0200

    add Small-Clothes page, Breadcrumbs

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,13 +1,31 @@
 import Link from 'next/link';
 
-/*TODO: add logic to breadcrumbs*/
+export type Crumb = {
+  text: string;
+  href?: string;
+};
 
-export default function Breadcrumbs() {
+export default function Breadcrumbs({ crumbs }: { crumbs: Crumb[] }) {
   return (
-    <div className='hidden py-3 text-sm text-gray-900 md:flex md:gap-1 lg:mb-1 lg:py-1.5 lg:text-base xl:mb-3'>
-      <Link href={'/'}>Головна</Link>
-      <span className='pr-1 text-base/[20px] font-bold lg:text-base'>/</span>
-      <span className='font-bold'>Каталог</span>
-    </div>
+    <ul className='hidden md:mb-2 md:flex md:gap-1 md:py-2 md:text-sm md:text-gray-900 lg:mb-1 xl:mb-3'>
+      <li>
+        <Link href={'/'}>Головна</Link>
+      </li>
+      {crumbs.map((crumb, index, arr) =>
+        index !== arr.length - 1 ? (
+          <li key={index} className='flex gap-2'>
+            <span>/</span>
+            <Link href={crumb.href!}>{crumb.text}</Link>
+          </li>
+        ) : (
+          <li key={index} className='flex gap-2'>
+            <span>/</span>
+            <span key={index} className='text-brand-grey-600 font-bold'>
+              {crumb.text}
+            </span>
+          </li>
+        )
+      )}
+    </ul>
   );
 }
