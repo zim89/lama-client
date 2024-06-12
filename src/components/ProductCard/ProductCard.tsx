d0c5ad8669852d5ffc57e75efd536addab8c2de3
zim89 <121241841+zim89@users.noreply.github.com>
2024-06-12 17:59:05 +0300

    fixed ProductCard, ProductList, SaleSection

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import Image from 'next/image'
+import Link from 'next/link'
 import AddToFavoritesBtn from '@/components/AddToFavoritesBtn'
-import { Rating } from '@/components/Rating'
 import type { Product } from '@/shared/types/product.types'
 import { cn, formatImageUrl } from '@/shared/lib/utils'
 import ColorSelect from '../ColorSelect'
@@ -17,57 +17,56 @@ export default function ProductCard({ product }: { product: Product }) {
   return (
     <div className={cn('group', styles.wrap)}>
       <AddToFavoritesBtn styles={styles.favButton} />
-      <div className={styles.imageWrap}>
-        <Image
-          src={formatImageUrl(product.product_images[0].product_image)}
-          alt={product.name}
-          fill
-          sizes='(min-width: 1024px) 25vw, (min-width: 768px) 33vw, 50vw'
-          className='object-cover'
-        />
-      </div>
+      <Link href={`products/${product.id}`}>
+        <div className={styles.imageWrap}>
+          <Image
+            src={formatImageUrl(product.product_images[0].product_image)}
+            alt={product.name}
+            fill
+            sizes='(min-width: 1024px) 25vw, (min-width: 768px) 33vw, 50vw'
+            className='object-cover'
+          />
+        </div>
+      </Link>
       <div
         className={cn(
           styles.content,
-          'group-hover:top-[166px] transition-all duration-300 xl:group-hover:top-[184px]'
+          'group-hover:top-[188px] transition-all duration-300 xl:group-hover:top-[212px]'
         )}
       >
-        <h3
-          className={cn(
-            styles.title,
-            'lg:group-hover:line-clamp-2 lg:group-hover:h-9 xl:group-hover:h-10'
-          )}
+        <Link
+          href={`products/${product.id}`}
+          className='space-y-1.5 lg:space-y-2.5 xl:space-y-3'
         >
-          {product.name}
-        </h3>
+          <h3
+            className={cn(
+              styles.title,
+              'lg:group-hover:line-clamp-2 lg:group-hover:h-10'
+            )}
+          >
+            {product.name}
+          </h3>
 
-        <Rating
-          rating={product.rating}
-          size={16}
-          className='hidden transition-all duration-300 group-hover:block'
-          showText={false}
-          disabled={true}
-        />
-        <div className='flex gap-1 items-baseline'>
-          {product.discount ? (
-            <>
+          <div className='flex gap-1 items-baseline'>
+            {product.discount ? (
+              <>
+                <p className={styles.textPrice}>
+                  від {formatPrice(Number(product.price))}
+                </p>
+                <p className={styles.textDiscount}>
+                  {formatPriceWithDiscount(
+                    product.price,
+                    product.discount.amount
+                  )}
+                </p>
+              </>
+            ) : (
               <p className={styles.textPrice}>
                 від {formatPrice(Number(product.price))}
               </p>
-              <p className={styles.textDiscount}>
-                {formatPriceWithDiscount(
-                  product.price,
-                  product.discount.amount
-                )}
-              </p>
-            </>
-          ) : (
-            <p className={styles.textPrice}>
-              від {formatPrice(Number(product.price))}
-            </p>
-          )}
-        </div>
-
+            )}
+          </div>
+        </Link>
         <ColorSelect colors={getColorsFromAttr(product.attributes)} />
       </div>
     </div>
--- src/components/ProductCard/ProductCardEmpty.tsx
+++ src/components/ProductCard/ProductCardEmpty.tsx
@@ -1,27 +1,19 @@
 'use client'
 
-import Image from 'next/image'
+import Link from 'next/link'
 import type { Product } from '@/shared/types/product.types'
-import { formatImageUrl } from '@/shared/lib/utils'
-import styles from './ProductCard.module.css'
+import { ChevronRightIcon } from '../IconsPack'
 
 export default function ProductCardEmpty({ product }: { product: Product }) {
   return (
-    <div className={styles.wrap}>
-      <div className={styles.imageWrap}>
-        <Image
-          src={formatImageUrl(product.product_images[0].product_image)}
-          alt={product.name}
-          fill
-          sizes='(min-width: 1024px) 25vw, (min-width: 768px) 33vw, 50vw'
-          className='object-cover'
-        />
-      </div>
-      <div className='absolute inset-0 z-10 flex items-end justify-center bg-white/40 pb-7'>
-        <button className='whitespace-nowrap rounded-xl bg-accent-500 px-5 py-3 text-sm font-medium transition-colors duration-300 hover:bg-accent-700'>
-          Дивитись все
-        </button>
-      </div>
+    <div className='flex justify-center items-center h-[252px] w-[164px] sm:h-[296px] sm:w-[210px] md:h-[310px] md:w-[229px] lg:h-[306px] lg:w-[225px] xl:h-[338px] xl:w-[282px]'>
+      <Link
+        href={'/'}
+        className='whitespace-nowrap btn-tertiary'
+      >
+        Дивитись все
+        <ChevronRightIcon />
+      </Link>
     </div>
   )
 }
